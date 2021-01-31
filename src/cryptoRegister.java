10df7a0c0cd652841051733af5c8de30f226d0ba
blue0print <adigoza@usc.edu>
2021-01-31 15:06:13 -0800

    Version 0.0

--- src/cryptoRegister.java
+++ src/cryptoRegister.java
@@ -4,22 +4,26 @@ public class cryptoRegister {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-        System.out.print("Coin name: ");
+        System.out.print("Three letter coin name: ");
         String coinName = in.next();
 
         System.out.print("Enter coin amount: ");
         double coinAmount = in.nextDouble();
         //System.out.println("");
 
-        System.out.print("Enter coin purchase price: ");
+        System.out.print("Enter coin purchase price [in USD]: ");
         double coinPurchasePrice = in.nextDouble();
         //System.out.println("");
 
         double totInvestment = coinAmount * coinPurchasePrice;
 
-        System.out.println("Coin Name       Coin Amount     Coin PP     Investment");
-        System.out.println("------------------------------------------------------");
-        System.out.println(coinName +"             " +coinAmount +"           $" +coinPurchasePrice +"     $" +totInvestment);
+        System.out.println("Coin Name       Coin Amount     Coin PP       Investment");
+        System.out.println("----------------------------------------------------------");
+        System.out.printf("%s             %.2f          $%.2f     $%.2f",coinName,coinAmount,
+                coinPurchasePrice,totInvestment);
+
+
+
 
     }
 }
