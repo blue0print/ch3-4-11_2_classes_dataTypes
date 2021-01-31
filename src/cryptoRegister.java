67b17d2034614d6395c4b51e55a02f0057563a34
blue0print <adigoza@usc.edu>
2021-01-31 14:16:52 -0800

    Version 0.0

---
+++ src/cryptoRegister.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class cryptoRegister {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Coin name: ");
+        String coinName = in.next();
+        System.out.println("Coin entered: " +coinName);
+
+
+    }
+}
